f02466642823b18ca6171b5704357d64552034eb
Kauê Martins <ksampaiosp@gmail.com>
2022-07-26 10:25:04 -0300

    refactor: remove print

--- packages/groveman_sentry/example/lib/main.dart
+++ packages/groveman_sentry/example/lib/main.dart
@@ -5,7 +5,6 @@ import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() {
   Groveman.plantTree(DebugTree(showColor: true));
-  print('DNS - ${const String.fromEnvironment('SENTRY_DNS')}');
   Groveman.captureErrorInZone(() async {
     await SentryFlutter.init(
       (options) {
